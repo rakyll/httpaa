938c5715856ddba7a7bd176c58eb28bf8217bfcf
JBD <jbd@golang.org>
2019-07-24 17:33:08 -0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rakyll/httpaa
+
+go 1.13
---
+++ httpaa.go
@@ -0,0 +1,58 @@
+// Copyright 2019 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package httpaa is like net/http but
+// provides a http.Handle function with
+// all others a's.
+//
+// This package is inspired by
+// https://twitter.com/chimeracoder/status/1154156377301929984.
+package httpaa
+
+import "net/http"
+
+// HaandleFunc is like http.HandleFunc but with aa.
+func HaandleFunc(pattern string, handler func(w http.ResponseWriter, r *http.Request)) {
+	http.HandleFunc(pattern, handler)
+}
+
+// HåndleFunc is like http.HandleFunc but with å.
+func HåndleFunc(pattern string, handler func(w http.ResponseWriter, r *http.Request)) {
+	http.HandleFunc(pattern, handler)
+}
+
+// HändleFunc is like http.HandleFunc but with ä.
+func HändleFunc(pattern string, handler func(w http.ResponseWriter, r *http.Request)) {
+	http.HandleFunc(pattern, handler)
+}
+
+// HândleFunc is like http.HandleFunc but with â.
+func HândleFunc(pattern string, handler func(w http.ResponseWriter, r *http.Request)) {
+	http.HandleFunc(pattern, handler)
+}
+
+// HàndleFunc is like http.HandleFunc but with à.
+func HàndleFunc(pattern string, handler func(w http.ResponseWriter, r *http.Request)) {
+	http.HandleFunc(pattern, handler)
+}
+
+// HándleFunc is like http.HandleFunc but with á.
+func HándleFunc(pattern string, handler func(w http.ResponseWriter, r *http.Request)) {
+	http.HandleFunc(pattern, handler)
+}
+
+// HædleFunc is like http.HandleFunc but with æ.
+func HædleFunc(pattern string, handler func(w http.ResponseWriter, r *http.Request)) {
+	http.HandleFunc(pattern, handler)
+}
